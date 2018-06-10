17e35d4d898c855ff346800dd5a0fa599e12907a
PicoSushi <picosushi12+git@gmail.com>
2018-06-10 22:47:28 +0900

    Divide files

--- meshi.go
+++ meshi.go
@@ -1,42 +1,38 @@
-package main
+package meshi
 
 import (
 	"log"
-	"os"
 
-	"github.com/kr/pretty"
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
 )
 
-const FREE_QUERY_PER_SECOND = 45
+const FREE_QUERY_PER_SECOND = 2
 
 func main() {
-	api_key := os.Getenv("GOOGLE_MAPS_API_KEY")
+}
 
+// meshi searches some restraunt with given name and returns
+func Meshi(api_key string, lat float64, lng float64, rad uint, keyword string) maps.PlacesSearchResponse {
 	c, err := maps.NewClient(maps.WithAPIKey(api_key), maps.WithRateLimit(FREE_QUERY_PER_SECOND))
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
 
-	shinjuku_station := &maps.LatLng{
-		Lat: 35.690921,
-		Lng: 139.70025799999996,
-	}
-
 	r := &maps.NearbySearchRequest{
-		Location: shinjuku_station,
-		Radius:   300,
-		Keyword:  "いちごパフェ",
-		OpenNow:  false,
+		Location: &maps.LatLng{
+			Lat: lat,
+			Lng: lng,
+		},
+		Radius:  rad,
+		Keyword: keyword,
+		Type:    maps.PlaceTypeRestaurant,
+		OpenNow: false,
 	}
 
 	response, err := c.NearbySearch(context.Background(), r)
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
-
-	for _, result := range response.Results {
-		pretty.Println(result.Name)
-	}
+	return response
 }
