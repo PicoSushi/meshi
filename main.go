fe00846c4557ec874446848d9d608238cc6ecc5a
PicoSushi <picosushi12+git@gmail.com>
2018-06-10 19:41:04 +0900

    Add sample code

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/kr/pretty"
+	"golang.org/x/net/context"
+	"googlemaps.github.io/maps"
+)
+
+const FREE_QUERY_PER_SECOND = 45
+
+func main() {
+	api_key := os.Getenv("GOOGLE_MAPS_API_KEY")
+
+	c, err := maps.NewClient(maps.WithAPIKey(api_key), maps.WithRateLimit(FREE_QUERY_PER_SECOND))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	shinjuku_station := &maps.LatLng{
+		Lat: 35.690921,
+		Lng: 139.70025799999996,
+	}
+	r := &maps.NearbySearchRequest{
+		Location: shinjuku_station,
+		Radius:   300,
+		Keyword:  "パフェ",
+		OpenNow:  true,
+	}
+
+	route, err := c.NearbySearch(context.Background(), r)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	pretty.Println(route)
+}
